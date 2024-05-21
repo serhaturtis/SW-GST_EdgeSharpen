08cbc99ddc7c59c6bd4289d8247bc9cc4510ab87
M. Serhat Urtis <urtisms@gmail.com>
2024-05-21 07:26:31 +0300

    Removed weird CR

--- gstedgesharpen.c
+++ gstedgesharpen.c
@@ -1,6 +1,5 @@
 /* GStreamer
- * Copyright (C) 2022 Serhat Urtis <urtisms@gmail.com>
- *
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
